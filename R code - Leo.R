6eca94a7a7d2ac1261e4fefd8afb0ae7314421f4
LeoSeror <150180866+LeoSeror@users.noreply.github.com>
2023-11-27 12:48:17 -0500

    R code - Leo.R

--- R code - Leo.R
+++ R code - Leo.R
@@ -1,36 +1,43 @@
-library(lubridate)
 library(dplyr)
 
-Flight_data$FL_DATE <- as.Date(Flight_data$FL_DATE)
+data <- Flights1_2019_1
 
-# Number of flights received per day
-flights_per_day <- Flight_data %>%
-  group_by(FL_DATE) %>%
-  summarise(NumFlights = n())
+# Ensure date is in the correct format (adjust the format as per your data)
+data$FL_DATE <- as.Date(data$FL_DATE, format="%Y-%m-%d")
 
-# Number of flights received per day
-flights_per_day <- Flight_data %>%
-  group_by(FL_DATE) %>%
-  summarise(NumFlights = n())
+# Add columns for day of the week and month
+data$month <- format(data$FL_DATE, "%m")
 
-# Average number of flights received by day of the week
-Flight_data$DAY_OF_WEEK <- weekdays(Flight_data$FL_DATE)
-avg_flights_day_of_week <- Flight_data %>%
-  group_by(DAY_OF_WEEK) %>%
-  summarise(AvgFlights = mean(n(), na.rm = TRUE))
+# Number of flights received per day by DEST_CITY_NAME
+daily_flights <- data %>%
+  group_by(DEST_CITY_NAME, FL_DATE) %>%
+  summarize(num_flights = n())
 
-# Average number of flights received by month
-Flight_data$Month <- month(Flight_data$FL_DATE)
-avg_flights_month <- Flight_data %>%
-  group_by(Month) %>%
-  summarise(AvgFlights = mean(n(), na.rm = TRUE))
+# Average number of flights received by day of the week by DEST_CITY_NAME
+weekly_avg_flights <- data %>%
+  group_by(DEST_CITY_NAME, DAY_OF_WEEK) %>%
+  summarize(total_flights = n())
 
-# Busiest month
-busiest_month <- avg_flights_month[which.max(avg_flights_month$AvgFlights), ]
+# Average number of flights received by month by DEST_CITY_NAME
+monthly_avg_flights <- data %>%
+  group_by(DEST_CITY_NAME, month) %>%
+  summarize(total_flights = n())
 
-# Busiest day of the week
-busiest_day_of_week <- avg_flights_day_of_week[which.max(avg_flights_day_of_week$AvgFlights), ]
+# Busiest month by DEST_CITY_NAME
+busiest_month <- monthly_avg_flights %>%
+  group_by(DEST_CITY_NAME) %>%
+  top_n(1, total_flights)
 
-# Average number of flights on the busiest day of the week
-Flight_data$NumFlights <- 1
-avg_flights_busiest_day <- mean(Flight_data[Flight_data$DAY_OF_WEEK == busiest_day_of_week$DAY_OF_WEEK, "NumFlights"], na.rm = TRUE)
+# Busiest day of the week by DEST_CITY_NAME
+busiest_day <- weekly_avg_flights %>%
+  group_by(DEST_CITY_NAME) %>%
+  top_n(1, total_flights)
+
+# Average number of flights on the busiest day of the week by DEST_CITY_NAME
+avg_flights_busiest_day <- busiest_day %>%
+  left_join(weekly_avg_flights, by = c("DEST_CITY_NAME", "DAY_OF_WEEK"))
+
+# Number of flights received by origin state by DEST_CITY_NAME
+flights_by_origin_state <- data %>%
+  group_by(DEST_CITY_NAME, ORIGIN_CITY_NAME) %>%
+  summarize(num_flights = n())
