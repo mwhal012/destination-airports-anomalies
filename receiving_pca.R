cdd26db9cae6541b365c4648f929541913a8e544
mwhal012 <145363243+mwhal012@users.noreply.github.com>
2023-11-27 23:31:22 -0500

    principal components data

---
+++ receiving_pca.R
@@ -0,0 +1,42 @@
+library(tidyverse)
+library(conflicted)
+"dplyr" |>
+  conflict_prefer_all(quiet = TRUE)
+
+data = "receiving_airports.csv" |>
+  read_csv(
+    col_types = cols(
+      DEST_AIRPORT_SEQ_ID = col_factor(),
+      busiest_day_of_week = col_factor(levels = c("1", "2", "3", "4", "5", "6", "7")),
+      most_common_origin_apt = col_factor()
+    )
+  ) |>
+  mutate(
+    where(is.numeric) |>
+      across(scale)
+  )
+
+dat_num = data |>
+  select(where(is.numeric)) |>
+  as.matrix()
+
+pca_data = dat_num |>
+  stats::prcomp()
+summary(pca_data)
+
+# DUDADS
+vals = eigen(t(dat_num) %*% dat_num)$values
+scree.y = vals / sum(vals)
+
+barplot(
+  scree.y,
+  ylab = "% explained",
+  xlab = "PC",
+  col = heat.colors(12)
+)
+test = seq(0.7, 13.9, length.out = ncol(dat_num))
+axis(1, at = test, labels = 1:ncol(dat_num)) # bend after 5th component
+
+pca_data$x[, 1:5] |>
+  as_tibble() |>
+  write_csv(file = "receiving_airports_pc.csv")
