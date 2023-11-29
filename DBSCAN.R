751034c146227d9ef12da8701f66563fd983b706
jcameron761 <150046434+jcameron761@users.noreply.github.com>
2023-11-29 13:49:36 -0500

    Add files via upload

--- DBSCAN.R
+++ DBSCAN.R
@@ -1,9 +1,11 @@
+setwd("C:/Users/jaybh/Documents/R Files/Data")
 library(dbscan)
 
 Receiving_Airports <- read.csv("receiving_airports_pc.csv")
-kNNdistplot(Receiving_Airports, minPts = 20)
+kNNdistplot(Receiving_Airports, minPts = 5)
 abline(h=15, col = "red",lty = 2)
-db <- dbscan(Receiving_Airports, eps = 10, minPts = 7)
+
+db <- dbscan(Receiving_Airports, eps = 10, minPts = 12)
 db
 pairs(Receiving_Airports, col = db$cluster + 1L)
 
